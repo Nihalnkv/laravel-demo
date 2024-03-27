04d99e3f988aac3ffe740d455d25b7f7bddb0036
Nihalnkv <nihalkv57@gmail.com>
2024-03-27 11:18:24 +0530

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Users;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function test(){
+        echo 'hello';
+    }
+}
